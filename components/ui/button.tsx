8fb8fdd6206346904f00dc5400992cdc6049d967
Kamran Nawaz <kamrannawaz015@gmail.com>
2024-01-03 22:55:12 +0500

    boardid page done, update board title done, 6.31 hours

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-0 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
@@ -16,9 +16,11 @@ const buttonVariants = cva(
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground border-transparent",
+        ghost:
+          "hover:bg-accent hover:text-accent-foreground border-transparent",
         link: "text-primary underline-offset-4 hover:underline",
-        primary: "bg-sky-700 text-primary-foreground hover:bg-sky-700/90"
+        primary: "bg-sky-700 text-primary-foreground hover:bg-sky-700/90",
+        transparent: "bg-transparent text-white hover:bg-white/20"
       },
       size: {
         default: "h-10 px-4 py-2",
