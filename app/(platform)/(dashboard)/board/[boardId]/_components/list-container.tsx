18e63d1f8ad8f6932b93d964006a12dcbd7df791
Kamran Nawaz <35919931+kamrannawaz@users.noreply.github.com>
2024-01-05 17:31:11 +0500

    board list updated, 7.40 hours done

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -1,7 +1,10 @@
 "use client";
 
+import { use, useEffect, useState } from "react";
+
 import { ListWithCards } from "@/types";
 import { ListForm } from "./list-form";
+import { ListItem } from "./list-item";
 
 interface ListContainerProps {
   boardId: string;
@@ -9,10 +12,19 @@ interface ListContainerProps {
 }
 
 export const ListContainer = ({ boardId, data }: ListContainerProps) => {
+  const [orderedData, setOrderedData] = useState(data);
+
+  useEffect(() => {
+    setOrderedData(data);
+  }, [data]);
+
   return (
-    <ol>
+    <ol className="flex gap-x-3 h-full">
+      {orderedData.map((list, index) => {
+        return <ListItem key={list.id} index={index} data={list} />;
+      })}
       <ListForm />
-      <div className="flex-shrink-0 w-1"/>
+      <div className="flex-shrink-0 w-1" />
     </ol>
   );
 };
