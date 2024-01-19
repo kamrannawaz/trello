bdae60205e83871522eb28d8252636a312e4216d
Kamran Nawaz <35919931+kamrannawaz@users.noreply.github.com>
2024-01-19 12:39:27 +0500

    modal header implemented with api and react query hook

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { Draggable, Droppable } from "@hello-pangea/dnd";
+import { useCardModal } from "@/hooks/use-card-modal";
+import { Draggable } from "@hello-pangea/dnd";
 
 import { Card } from "@prisma/client";
 
@@ -10,6 +11,7 @@ interface CardItemProps {
 }
 
 export const CardItem = ({ index, data }: CardItemProps) => {
+  const cardModal = useCardModal();
   return (
     <Draggable draggableId={data.id} index={index}>
       {(provided) => (
@@ -18,6 +20,7 @@ export const CardItem = ({ index, data }: CardItemProps) => {
           {...provided.dragHandleProps}
           ref={provided.innerRef}
           role="button"
+          onClick={() => cardModal.onOpen(data.id)}
           className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm"
         >
           {data.title}
